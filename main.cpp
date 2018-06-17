1fcb030d18c6c028ef97c977a11a5b3ebc60d4d9
Slyracoon23 <33297016+Slyracoon23@users.noreply.github.com>
2018-06-17 19:02:42 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,27 +3,24 @@
 using namespace std;
 
 
-void some_function(){
+void increment()
+{
+int v[] = {0,1,2,3,4,5,6,7,8,9};
+    for (auto & x : v){
+        ++x;
+    }
     
-    double d =2.2;
-    
-    
-    int i = 7;
-    
-    d = d+i;
-    
-    i = d*i;
-    
-    
-    
-    
+    for (int x : v) {
+        cout << x << endl;
+    }
 }
+
+
 
 
 int main(int argc, char *argv[])
 {
-    
-    some_function();
+    increment();
     
     return 0;
 }
